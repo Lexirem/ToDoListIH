577e07c53701c03de89a34bd93177f1a5a0145e8
Txell <meritxell@gold.barcelona>
2022-09-13 19:48:23 +0200

    signUp and signIn forms done and working. working on confirming password

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,30 +3,48 @@
     <h2><b>Sign Up</b></h2>
     <form class="form" method="post" @submit.prevent="handleSignUp">
       <label for="email">Email:
-        <input type="email" v-model="emailValue" placeholder="Your email">
+        <input type="email" v-model="email" placeholder="Your email">
       </label>
       <label for="password">Password:
-        <input type="password" v-model="passwordValue" placeholder="********">
+        <input type="password" v-model="password" placeholder="********">
       </label>
       <label for="password">Confirm your Password:
-        <input type="password" v-model="passwordValue" placeholder="********">
+        <input type="password" v-model="confirmPassword" placeholder="********">
       </label>
       <div v-if="passwordError">{{ passwordError }}</div>
-      <button @click="goToSignIn">Already a user? Sign In</button>
+      <button @click.prevent="goToSignIn">Already a user? Sign In</button>
+      <button @click.prevent="handleSignUp">Sign Up</button>
     </form>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'pinia';
+import userStore from '@/store/user';
+
 export default {
   name: 'signUp',
   data() {
     return {
       email: '',
       password: '',
+      confirmPassword: '',
     };
   },
+  computed: {
+    ...mapState(userStore, ['user']),
+  },
   methods: {
+    ...mapActions(userStore, ['signUp']),
+    handleSignUp() {
+      this.signUp(this.email, this.password, this.confirmPassword);
+      this.confirmPassword === this.password;
+      this.passwordError = this.password.length > 6 ? '' : 'Password should have more than 6 characters';
+      if (!this.passwordError) {
+        console.log(this.email);
+        console.log(this.password);
+      }
+    },
     goToSignIn() {
       this.$emit('modifyChangeForm', 'signIn');
     },
